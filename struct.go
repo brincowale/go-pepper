0d5e26af7a87a027d8d36ca3c2abe29ab8ae9104
IDerr <ibrahim@derraz.fr>
2019-12-26 22:59:42 +0100

    add sub structs

--- struct.go
+++ struct.go
@@ -1,138 +1,132 @@
 package dealabs
 
+type Data struct {
+	DealType          string  `json:"_type"`
+	Representation    string  `json:"_representation"`
+	ThreadID          int     `json:"thread_id"`
+	Description       string  `json:"description"`
+	Title             string  `json:"title"`
+	Origin            string  `json:"origin"`
+	Submitted         int     `json:"submitted"`
+	Updated           int     `json:"updated"`
+	Expired           bool    `json:"expired"`
+	Status            string  `json:"status"`
+	Local             bool    `json:"local"`
+	LastCommented     int     `json:"last_commented"`
+	CommentCount      int     `json:"comment_count"`
+	GroupCount        int     `json:"group_count"`
+	Price             float64 `json:"price,omitempty"`
+	DealURI           string  `json:"deal_uri"`
+	VisitURI          string  `json:"visit_uri,omitempty"`
+	ShortURI          string  `json:"short_uri"`
+	TemperatureRating int     `json:"temperature_rating"`
+	TemperatureLevel  string  `json:"temperature_level"`
+	IsHot             bool    `json:"is_hot"`
+	Type              struct {
+		Type              string `json:"_type"`
+		Representation    string `json:"_representation"`
+		TypeID            int    `json:"type_id"`
+		Name              string `json:"name"`
+		ShortName         string `json:"short_name"`
+		DisplayOrder      int    `json:"display_order"`
+		ThreadTypeURLName string `json:"thread_type_url_name"`
+		Dealable          bool   `json:"dealable"`
+		TypeUniversalID   string `json:"type_universal_id"`
+		TopicFilterable   bool   `json:"topic_filterable"`
+		Filterable        bool   `json:"filterable"`
+	} `json:"type"`
+	NextBestPrice        float64 `json:"next_best_price,omitempty"`
+	PriceDiscount        int     `json:"price_discount,omitempty"`
+	Flaggable            bool    `json:"flaggable"`
+	Votable              bool    `json:"votable"`
+	Editable             bool    `json:"editable"`
+	GroupDisplaySummary  string  `json:"group_display_summary"`
+	ShowBumpedStatus     bool    `json:"show_bumped_status"`
+	DisplayPriceAsFree   bool    `json:"display_price_as_free,omitempty"`
+	HasSuggestionCards   bool    `json:"has_suggestion_cards"`
+	IconListURL          string  `json:"icon_list_url"`
+	IconDetailURL        string  `json:"icon_detail_url"`
+	HasSuggestedKeywords bool    `json:"has_suggested_keywords"`
+	Clearance            bool    `json:"clearance"`
+	Featured             int     `json:"featured"`
+	Created              int     `json:"created"`
+	IsNew                bool    `json:"is_new"`
+	IsNsfw               bool    `json:"is_nsfw"`
+	Image                Image   `json:"image"`
+	MainGroup            struct {
+		Type       string `json:"_type"`
+		GroupID    int    `json:"group_id"`
+		Name       string `json:"name"`
+		URLName    string `json:"url_name"`
+		Default    bool   `json:"default"`
+		Followable bool   `json:"followable"`
+		Followed   bool   `json:"followed"`
+		Statistics struct {
+			ActiveDealCount    int `json:"active_deal_count"`
+			ActiveVoucherCount int `json:"active_voucher_count"`
+			CommentCount       int `json:"comment_count"`
+			ThreadCount        int `json:"thread_count"`
+		} `json:"statistics"`
+		SubmittableThreadTypes  []int    `json:"submittable_thread_types"`
+		Sections                []string `json:"sections"`
+		PepperURL               string   `json:"pepper_url"`
+		HeaderDescription       string   `json:"header_description"`
+		HeroBannerSmartphoneURL string   `json:"hero_banner_smartphone_url"`
+		HeroBannerTabletURL     string   `json:"hero_banner_tablet_url"`
+		IconListURL             string   `json:"icon_list_url"`
+		IconDetailURL           string   `json:"icon_detail_url"`
+		Image                   Image    `json:"image"`
+	} `json:"main_group"`
+	Merchant             Merchant `json:"merchant,omitempty"`
+	AreShippingCostsFree bool     `json:"are_shipping_costs_free,omitempty"`
+	ShippingCosts        float64  `json:"shipping_costs,omitempty"`
+	Poster               Poster   `json:"poster,omitempty"`
+	ExpiryDate           int      `json:"expiry_date,omitempty"`
+}
+
+type Image struct {
+	Type        string `json:"_type"`
+	ImageID     int    `json:"image_id"`
+	URI         string `json:"uri"`
+	LargeURI    string `json:"large_uri"`
+	LargeWidth  int    `json:"large_width"`
+	LargeHeight int    `json:"large_height"`
+}
+
+type Poster struct {
+	Type           string `json:"_type"`
+	Representation string `json:"_representation"`
+	UserID         int    `json:"user_id"`
+	Username       string `json:"username"`
+	Followable     bool   `json:"followable"`
+	Title          string `json:"title"`
+	Status         string `json:"status"`
+	IconListURL    string `json:"icon_list_url"`
+	IconDetailURL  string `json:"icon_detail_url"`
+	Avatar         Image  `json:"avatar"`
+}
+type Merchant struct {
+	Type                string `json:"_type"`
+	MerchantID          int    `json:"merchant_id"`
+	Name                string `json:"name"`
+	URLName             string `json:"url_name"`
+	PepperURL           string `json:"pepper_url"`
+	MerchantExternalURL string `json:"merchant_external_url"`
+	HeaderDescription   string `json:"header_description"`
+	IconListURL         string `json:"icon_list_url"`
+	IconDetailURL       string `json:"icon_detail_url"`
+	Image               struct {
+		Type        string `json:"_type"`
+		ImageID     int    `json:"image_id"`
+		URI         string `json:"uri"`
+		LargeURI    string `json:"large_uri"`
+		LargeWidth  int    `json:"large_width"`
+		LargeHeight int    `json:"large_height"`
+	} `json:"image"`
+}
+
 type Deals struct {
-	Data []struct {
-		DealType          string  `json:"_type"`
-		Representation    string  `json:"_representation"`
-		ThreadID          int     `json:"thread_id"`
-		Description       string  `json:"description"`
-		Title             string  `json:"title"`
-		Origin            string  `json:"origin"`
-		Submitted         int     `json:"submitted"`
-		Updated           int     `json:"updated"`
-		Expired           bool    `json:"expired"`
-		Status            string  `json:"status"`
-		Local             bool    `json:"local"`
-		LastCommented     int     `json:"last_commented"`
-		CommentCount      int     `json:"comment_count"`
-		GroupCount        int     `json:"group_count"`
-		Price             float64 `json:"price,omitempty"`
-		DealURI           string  `json:"deal_uri"`
-		VisitURI          string  `json:"visit_uri,omitempty"`
-		ShortURI          string  `json:"short_uri"`
-		TemperatureRating int     `json:"temperature_rating"`
-		TemperatureLevel  string  `json:"temperature_level"`
-		IsHot             bool    `json:"is_hot"`
-		Type              struct {
-			Type              string `json:"_type"`
-			Representation    string `json:"_representation"`
-			TypeID            int    `json:"type_id"`
-			Name              string `json:"name"`
-			ShortName         string `json:"short_name"`
-			DisplayOrder      int    `json:"display_order"`
-			ThreadTypeURLName string `json:"thread_type_url_name"`
-			Dealable          bool   `json:"dealable"`
-			TypeUniversalID   string `json:"type_universal_id"`
-			TopicFilterable   bool   `json:"topic_filterable"`
-			Filterable        bool   `json:"filterable"`
-		} `json:"type"`
-		NextBestPrice        float64 `json:"next_best_price,omitempty"`
-		PriceDiscount        int     `json:"price_discount,omitempty"`
-		Flaggable            bool    `json:"flaggable"`
-		Votable              bool    `json:"votable"`
-		Editable             bool    `json:"editable"`
-		GroupDisplaySummary  string  `json:"group_display_summary"`
-		ShowBumpedStatus     bool    `json:"show_bumped_status"`
-		DisplayPriceAsFree   bool    `json:"display_price_as_free,omitempty"`
-		HasSuggestionCards   bool    `json:"has_suggestion_cards"`
-		IconListURL          string  `json:"icon_list_url"`
-		IconDetailURL        string  `json:"icon_detail_url"`
-		HasSuggestedKeywords bool    `json:"has_suggested_keywords"`
-		Clearance            bool    `json:"clearance"`
-		Featured             int     `json:"featured"`
-		Created              int     `json:"created"`
-		IsNew                bool    `json:"is_new"`
-		IsNsfw               bool    `json:"is_nsfw"`
-		Image                struct {
-			Type        string `json:"_type"`
-			ImageID     int    `json:"image_id"`
-			URI         string `json:"uri"`
-			LargeURI    string `json:"large_uri"`
-			LargeWidth  int    `json:"large_width"`
-			LargeHeight int    `json:"large_height"`
-		} `json:"image"`
-		MainGroup struct {
-			Type       string `json:"_type"`
-			GroupID    int    `json:"group_id"`
-			Name       string `json:"name"`
-			URLName    string `json:"url_name"`
-			Default    bool   `json:"default"`
-			Followable bool   `json:"followable"`
-			Followed   bool   `json:"followed"`
-			Statistics struct {
-				ActiveDealCount    int `json:"active_deal_count"`
-				ActiveVoucherCount int `json:"active_voucher_count"`
-				CommentCount       int `json:"comment_count"`
-				ThreadCount        int `json:"thread_count"`
-			} `json:"statistics"`
-			SubmittableThreadTypes  []int    `json:"submittable_thread_types"`
-			Sections                []string `json:"sections"`
-			PepperURL               string   `json:"pepper_url"`
-			HeaderDescription       string   `json:"header_description"`
-			HeroBannerSmartphoneURL string   `json:"hero_banner_smartphone_url"`
-			HeroBannerTabletURL     string   `json:"hero_banner_tablet_url"`
-			IconListURL             string   `json:"icon_list_url"`
-			IconDetailURL           string   `json:"icon_detail_url"`
-			Image                   struct {
-				Type        string `json:"_type"`
-				ImageID     int    `json:"image_id"`
-				URI         string `json:"uri"`
-				LargeURI    string `json:"large_uri"`
-				LargeWidth  int    `json:"large_width"`
-				LargeHeight int    `json:"large_height"`
-			} `json:"image"`
-		} `json:"main_group"`
-		Merchant struct {
-			Type                string `json:"_type"`
-			MerchantID          int    `json:"merchant_id"`
-			Name                string `json:"name"`
-			URLName             string `json:"url_name"`
-			PepperURL           string `json:"pepper_url"`
-			MerchantExternalURL string `json:"merchant_external_url"`
-			HeaderDescription   string `json:"header_description"`
-			IconListURL         string `json:"icon_list_url"`
-			IconDetailURL       string `json:"icon_detail_url"`
-			Image               struct {
-				Type        string `json:"_type"`
-				ImageID     int    `json:"image_id"`
-				URI         string `json:"uri"`
-				LargeURI    string `json:"large_uri"`
-				LargeWidth  int    `json:"large_width"`
-				LargeHeight int    `json:"large_height"`
-			} `json:"image"`
-		} `json:"merchant,omitempty"`
-		AreShippingCostsFree bool    `json:"are_shipping_costs_free,omitempty"`
-		ShippingCosts        float64 `json:"shipping_costs,omitempty"`
-		Poster               struct {
-			Type           string `json:"_type"`
-			Representation string `json:"_representation"`
-			UserID         int    `json:"user_id"`
-			Username       string `json:"username"`
-			Followable     bool   `json:"followable"`
-			Status         string `json:"status"`
-			IconListURL    string `json:"icon_list_url"`
-			IconDetailURL  string `json:"icon_detail_url"`
-			Avatar         struct {
-				Type        string `json:"_type"`
-				ImageID     int    `json:"image_id"`
-				URI         string `json:"uri"`
-				LargeURI    string `json:"large_uri"`
-				LargeWidth  int    `json:"large_width"`
-				LargeHeight int    `json:"large_height"`
-			} `json:"avatar"`
-		} `json:"poster,omitempty"`
-		ExpiryDate int `json:"expiry_date,omitempty"`
-	} `json:"data"`
+	Data       []Data        `json:"data"`
 	Messages   []interface{} `json:"messages"`
 	Validation []interface{} `json:"validation"`
 	Cursors    struct {
@@ -197,34 +191,9 @@ type Deals struct {
 			Created              int    `json:"created"`
 			IsNew                bool   `json:"is_new"`
 			IsNsfw               bool   `json:"is_nsfw"`
-			Poster               struct {
-				Type           string `json:"_type"`
-				Representation string `json:"_representation"`
-				UserID         int    `json:"user_id"`
-				Username       string `json:"username"`
-				Followable     bool   `json:"followable"`
-				Title          string `json:"title"`
-				Status         string `json:"status"`
-				IconListURL    string `json:"icon_list_url"`
-				IconDetailURL  string `json:"icon_detail_url"`
-				Avatar         struct {
-					Type        string `json:"_type"`
-					ImageID     int    `json:"image_id"`
-					URI         string `json:"uri"`
-					LargeURI    string `json:"large_uri"`
-					LargeWidth  int    `json:"large_width"`
-					LargeHeight int    `json:"large_height"`
-				} `json:"avatar"`
-			} `json:"poster"`
-			Image struct {
-				Type        string `json:"_type"`
-				ImageID     int    `json:"image_id"`
-				URI         string `json:"uri"`
-				LargeURI    string `json:"large_uri"`
-				LargeWidth  int    `json:"large_width"`
-				LargeHeight int    `json:"large_height"`
-			} `json:"image"`
-			MainGroup struct {
+			Poster               Poster `json:"poster"`
+			Image                Image  `json:"image"`
+			MainGroup            struct {
 				Type       string `json:"_type"`
 				GroupID    int    `json:"group_id"`
 				Name       string `json:"name"`
@@ -246,34 +215,9 @@ type Deals struct {
 				HeroBannerTabletURL     string   `json:"hero_banner_tablet_url"`
 				IconListURL             string   `json:"icon_list_url"`
 				IconDetailURL           string   `json:"icon_detail_url"`
-				Image                   struct {
-					Type        string `json:"_type"`
-					ImageID     int    `json:"image_id"`
-					URI         string `json:"uri"`
-					LargeURI    string `json:"large_uri"`
-					LargeWidth  int    `json:"large_width"`
-					LargeHeight int    `json:"large_height"`
-				} `json:"image"`
+				Image                   Image    `json:"image"`
 			} `json:"main_group"`
-			Merchant struct {
-				Type                string `json:"_type"`
-				MerchantID          int    `json:"merchant_id"`
-				Name                string `json:"name"`
-				URLName             string `json:"url_name"`
-				PepperURL           string `json:"pepper_url"`
-				MerchantExternalURL string `json:"merchant_external_url"`
-				HeaderDescription   string `json:"header_description"`
-				IconListURL         string `json:"icon_list_url"`
-				IconDetailURL       string `json:"icon_detail_url"`
-				Image               struct {
-					Type        string `json:"_type"`
-					ImageID     int    `json:"image_id"`
-					URI         string `json:"uri"`
-					LargeURI    string `json:"large_uri"`
-					LargeWidth  int    `json:"large_width"`
-					LargeHeight int    `json:"large_height"`
-				} `json:"image"`
-			} `json:"merchant"`
+			Merchant   Merchant `json:"merchant"`
 			PinnedInfo struct {
 				Type            string `json:"_type"`
 				Position        int    `json:"position"`
